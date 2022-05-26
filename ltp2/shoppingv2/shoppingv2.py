ced507d95e938a5be6c7271d589d0099b5bf60a7
atamsekar <atamsekar@pulsesecure.net>
2022-05-25 18:23:56 -0700

    updated shoppingv2.py with proper output format.

--- ltp2/shoppingv2/shoppingv2.py
+++ ltp2/shoppingv2/shoppingv2.py
@@ -10,5 +10,12 @@ while not order.quit:
     cart.process(order)
     order = sales.shopping_order.Order()
     order.get_input()
-    
-print(cart)
+
+
+cart_message = "Items in your Cart.."
+print(cart_message)
+print('-' * len(cart_message))
+print("{:<10} {:<10}".format('Items', 'Quantity'))
+for key in cart._contents:
+    items,quantity = key,cart._contents[key]
+    print("{:<10} {:<10}".format(items,quantity))
